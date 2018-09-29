507607186351535e9854636de3858862d63ad758
Danny Ouellet <danny_ouellet@hotmail.com>
2018-09-28 23:13:25 -0400

    init projet

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+					<link href="assets/styles.css" rel="stylesheet">
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+			<style>
+				
+			</style>
+    </head>
+
+    <body>
+			 <nav class="nav-extended">
+
+					<div class="nav-content">
+						<ul class="tabs tabs-transparent">
+							<li class="tab"><a href="#test1">Test 1</a></li>
+							<li class="tab"><a class="active" href="#test2">Test 2</a></li>
+							<li class="tab"><a href="#test4">Test 4</a></li>
+						</ul>
+					</div>
+				</nav>
+
+				<ul class="sidenav" id="mobile-demo">
+					<li><a href="sass.html">Sass</a></li>
+					<li><a href="badges.html">Components</a></li>
+					<li><a href="collapsible.html">JavaScript</a></li>
+				</ul>
+
+				<div id="test1" class="col s12">Test 1</div>
+				<div id="test2" class="col s12">Test 2</div>
+				<div id="test4" class="col s12">Test 4</div>
+	<!--JavaScript at end of body for optimized loading-->
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+	<script src="assets/scripts.js"></script>
+    </body>
+  </html>
