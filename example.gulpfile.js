991b80606d709617bfcc9953e8d9fbf0b87fa38c
Danny Ouellet <danny_ouellet@hotmail.com>
2018-09-30 08:37:03 -0400

    a jour gulp

--- example.gulpfile.js
+++ example.gulpfile.js
@@ -1,17 +1,23 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
 
 // Static Server + watching scss/html files
-gulp.task('serve', function() {
+gulp.task('serve', ['css'], function() {
 
     browserSync.init({
-        proxy: "http://vvv.test/eventshack/"
+        proxy: "http://eventshack.test/"
     });
 
     gulp.watch("**/*.php").on('change', browserSync.reload);
-    gulp.watch("**/*.css").on('change', browserSync.stream);
-    gulp.watch("**/*.js").on('change', browserSync.reload);
+    gulp.watch("assets/**/*.scss", ['css']);
+    gulp.watch("assets/**/*.js").on('change', browserSync.reload);
 });
-
+gulp.task('css', function() {
+    return gulp.src("assets/scss/styles.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("assets"))
+        .pipe(browserSync.stream());
+});
 
 gulp.task('default', ['serve']);
